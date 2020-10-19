0aac59c65de2094531a677327e8c59aa8f7d930b
ybq <hello@ybq.com>
2020-10-19 21:27:41 +0800

    修改工具类

--- src/main/java/com/orangelala/www/utils/ResponseUtils.java
+++ src/main/java/com/orangelala/www/utils/ResponseUtils.java
@@ -6,9 +6,11 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class ResponseUtils {
+    public static final String CHARACTER_ENCODING = "utf-8";
+    public static final String CONTENT_TYPE = "application/json; charset=utf-8";
     public static void responseToJson(HttpServletResponse response, Object object) throws IOException {
-        response.setCharacterEncoding("utf-8");
-        response.setContentType("application/json; charset=utf-8");
+        response.setCharacterEncoding(CHARACTER_ENCODING);
+        response.setContentType(CONTENT_TYPE);
         response.getWriter().println(JSONObject.toJSONString(object));
     }
 }
