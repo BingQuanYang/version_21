ab187ce65a6e8738de67becc57a3b58e41f00f8d
ybq <hello@ybq.com>
2020-10-20 16:58:27 +0800

    添加实体类Category

---
+++ src/main/java/com/orangelala/www/entity/Category.java
@@ -0,0 +1,37 @@
+package com.orangelala.www.entity;
+
+
+public class Category {
+
+  private String id;
+  private String name;
+  private String describe;
+
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+
+  public String getDescribe() {
+    return describe;
+  }
+
+  public void setDescribe(String describe) {
+    this.describe = describe;
+  }
+
+}
