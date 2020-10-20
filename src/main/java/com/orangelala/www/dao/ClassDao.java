6d672a1d871242ca64c6b27e310e5f74064dad83
yingzshan <m13129197823@163.com>
2020-10-20 17:25:32 +0800

    修改dao层接口，添加功能

--- src/main/java/com/orangelala/www/dao/ClassDao.java
+++ src/main/java/com/orangelala/www/dao/ClassDao.java
@@ -1,5 +1,6 @@
 package com.orangelala.www.dao;
 import com.orangelala.www.entity.Classification;
+import com.orangelala.www.entity.Headlines;
 
 import java.util.List;
 
@@ -12,4 +13,9 @@ public interface ClassDao {
      * @return 分类信息
      */
     List<Classification> selectClassInformation();
+    /**
+     * 查询头条信息
+     * @return 头条信息
+     */
+    List<Headlines> selectHeadlinesInformation();
 }
