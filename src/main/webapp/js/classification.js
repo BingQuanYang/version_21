af7a5ccbabdd2326844c9d869fd349269a698549
ybq <hello@ybq.com>
2020-10-20 14:54:25 +0800

    修改首页的js

--- src/main/webapp/js/classification.js
+++ src/main/webapp/js/classification.js
@@ -4,17 +4,23 @@
  */
 
 const BASE_URL = "http://localhost:8080/api"
-let INDEX_URL = BASE_URL + "/home2"
+let INDEX_URL = BASE_URL + "/HomeController"
 const IMAGE = "../images/cake.png"
 
 $(function () {
     $.get(INDEX_URL, function (result) {
         if (result.status === 200) {
-            showClassification(result.data.classes)
+            showClassification(result.data.classes);
+            showSeckill(result.data.seckill);
         } else {
             alert("系统繁忙!请稍后再试!!!")
         }
     });
+
+    /**
+     * 作者：kun
+     * @param classes
+     */
     function showClassification(classes) {
         for (const c of classes) {
             $("#js_climit_li")
@@ -53,6 +59,33 @@ $(function () {
                             )*/
                         )
 
+            )
+        }
+    }
+    function showSeckill(seckills){
+        for (const seckill of seckills) {
+            $("#sale").append(
+                $('<div class="am-u-sm-3 sale-item">').append(
+                    $('<div class="s-img">').append(
+                        $("<a href='../home/introduction.html?commodity_id="+seckill.pId+"'>").append(
+                            $('<img src="../images/'+seckill.image+'"/>')
+                        )
+                    )
+                ).append(
+                    $('<div class="s-info">').append(
+                        $("<a href='../home/introduction.html?commodity_id="+seckill.pId+"'>").append(
+                            $('<p class="s-title">').text(seckill.shopDiscount)
+                        )
+                    ).append(
+                        $('<div class="s-price">').append(
+                            "￥"
+                        ).append(
+                            $('<b>').text(seckill.originalPrice)
+                        ).append(
+                            $('<a class="s-buy" href="../home/introduction.html?commodity_id='+seckill.pId+'">秒杀</a>')
+                        )
+                    )
+                )
             )
         }
     }
