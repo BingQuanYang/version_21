cfc0505868821b3a0f05e7947267b0fa98dc2c56
lishenghong07 <1308072995@qq.com>
2020-10-21 20:29:15 +0800

    修改商品详情页面中图片的Dao层接口服务层接口(动态图)

--- src/main/java/com/orangelala/www/service/InformationService.java
+++ src/main/java/com/orangelala/www/service/InformationService.java
@@ -4,5 +4,8 @@ import com.orangelala.www.entity.Information;
 import com.orangelala.www.utils.DetailData;
 
 public interface InformationService {
+    //通过id获取商品的详细信息（一条）
     DetailData getIntroductionById(String id);
+    //通过id获取商品的推荐信息（集合）
+    DetailData getIntroductions(String id);
 }
