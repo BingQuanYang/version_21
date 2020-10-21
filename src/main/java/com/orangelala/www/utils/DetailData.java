ce1fd680e7fed6c71515d8018ba21921133d83c8
lishenghong07 <1308072995@qq.com>
2020-10-21 11:26:16 +0800

    修改

--- src/main/java/com/orangelala/www/utils/DetailData.java
+++ src/main/java/com/orangelala/www/utils/DetailData.java
@@ -2,12 +2,22 @@ package com.orangelala.www.utils;
 
 import com.orangelala.www.entity.Details;
 import com.orangelala.www.entity.Img;
+import com.orangelala.www.entity.Information;
 
 import java.util.List;
 
 public class DetailData {
+    public Information infor;
     public Details details;
     public List<Img> img;
+
+    public Information getInfor() {
+        return infor;
+    }
+
+    public void setInfor(Information infor) {
+        this.infor = infor;
+    }
 
     public Details getDetails() {
         return details;
